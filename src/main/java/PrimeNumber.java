e19f2a1355d956b1c10ae584cf523f4b7d4e2a38
lskalacheva2020 <lskalacheva2020@gmail.com>
2020-12-01 00:15:48 +0400

    add primenumber class

---
+++ src/main/java/PrimeNumber.java
@@ -0,0 +1,21 @@
+import java.*;
+
+import static java.lang.System.out;
+public class PrimeNumber {
+    public static void main(String[] args) {
+        {
+            for (int i = 2; i <= 100; i++) {
+                int isPrime = 1;
+                for (int j = 2; j < i; j++) {
+                    if (i % j == 0) {
+                        isPrime = 0;
+                        break;
+                    }
+                }
+                if (isPrime == 1) {
+                    System.out.println(i);;
+                }
+            }
+        }
+    }
+}
