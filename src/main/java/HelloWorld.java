86fcbd5da7c360ff6da91c5bb6a0669e20b23bbf
lskalacheva2020 <lskalacheva2020@gmail.com>
2021-02-08 22:26:29 +0400

    add new methods

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -1,13 +1,21 @@
+import java.util.Scanner;
+
 class App {
+
     public static void main(String[] args) {
-//        System.out.println("Hello, World!");
-//        System.out.println(12345 * 432);
-//        System.out.println("ONE\nTWO\nTHREE");
-//        printNumbersFromOneToTen();
-//        printNumbersFromOneToHundred();
-//        printOddNumbers();
-          printEvenNumbers();
+     //  System.out.println("Hello, World!");
+      //  System.out.println(12345 * 432);
+        //System.out.println("ONE\nTWO\nTHREE");
+       // printNumbersFromOneToTen();
+       // printNumbersFromOneToHundred();
+       // printOddNumbers();
+       // printEvenNumbers();
+        //fooBar();
+        //scannerNumberOneToN();
+       //scannerNumberAToB();
     }
+
+
 
     public static void printNumbersFromOneToTen() {
         int i = 1;
@@ -44,7 +52,50 @@ class App {
             i = i + 1;
         }
     }
+
+
+
+    public static void fooBar() {
+        int a = 1;
+        while (a <= 100) {
+            if (a % 3 == 0 && a % 5 == 0){
+                System.out.println("FOOBAR");
+            }
+            else if (a % 3 == 0) {
+                System.out.println("FOO");
+            }
+            else if (a % 5 == 0){
+                System.out.println("BAR");
+            }
+
+            a = a + 1;
+        }
+    }
+
+    public static void scannerNumberOneToN() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите число!");
+        int number = scanner.nextInt();
+        int a = 1;
+        while (a <= number) {
+            System.out.println(a);
+            a = a + 1;
+
+        }
+    }
+
+    public static void scannerNumberAToB() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите первое число!");
+        int a = scanner.nextInt();
+        System.out.println("Введите второе число!");
+        int b = scanner.nextInt();
+        while (a <= b) {
+            System.out.println(a);
+            a = a + 1;
+
+        }
+    }
+
+
 }
-
-
-
